d29e64d3d5bc63ae38fada62428fa6d6b805ec03
simplon-amel <amel.sid@hotmail.fr>
2015-12-14 14:45:30 +0100

    correction sur le bouton depart

--- pomo.js
+++ pomo.js
@@ -6,7 +6,7 @@ var taches;
 
 
 /*function test(){
-  console.log('marche');
+console.log('marche');
 taches = document.getElementById('formGroupInputLarge').value;
 finTaches = document.getElementById('terminer');
 li = document.createElement('li');
@@ -21,6 +21,7 @@ var min;
 var scs;
 var mls;
 var tab=[];
+var currentTime = false;
 
 function start(){
   console.log('marche');
@@ -33,143 +34,115 @@ function start(){
   min = 1;
   scs = 59;
   mls= 59;
- tab =[];
-time  =setInterval(function (){
+  tab = [];
+  time  =setInterval(function (){
+    timer = new Date();
+    document.getElementById('timer').innerHTML = min +":"+scs;
+    document.getElementById('depart').style.display ="none";
+    scs--;
+    if( scs < 0){
+      min--;
+      scs= 59;
+      if( min == 0){
+        min= 0;
 
- timer = new Date();
-
-
-document.getElementById('timer').innerHTML = min +":"+scs;
-
-  scs--;
-  if( scs < 0){
-    min--;
-    scs= 59;
-    if( min == 0){
-      min= 0;
+      }
+      if( min < 0){
+        min = 0;
+        clearInterval(time);
+        li.classList.add("selected");
+        document.getElementById('depart').style.display ="inline-block";
+        console.log( li + " est terminer");
+      }
     }
-    if( min < 0){
-      min = 0;
-      clearInterval(time);
-
-    li.classList.add("selected");
-    taches="";
-    }
-  }
-
-
-
-}, 1000);
-
+  }, 1000);
+  console.log("Start :" + min +":"+ scs)
 }
 
 
 
 function pause (){
-
   clearInterval(time);
-
   tab.push(min, scs);
-
   console.log(tab);
-
 }
 
 
 function reset(){
-  console.log("test");
+  tab = [];
+  console.log("reset enrgistre : "+ tab);
   clearInterval(time);
   document.getElementById('timer').innerText="00:00";
-
+  document.getElementById('depart').style.display ="inline-block";
 }
 
 function play(){
-
-
   min =tab[0];
   scs=tab[1];
-
   tab = [];
-
   time  =setInterval(function (){
-
- timer = new Date();
-
-
-document.getElementById('timer').innerHTML = min +":"+scs;
-
-  scs--;
-  if( scs < 0){
-    min--;
-    scs= 59;
-    if( min == 0){
-      min= 0;
-
+    timer = new Date();
+    document.getElementById('timer').innerHTML = min +":"+scs;
+    scs--;
+    if( scs < 0){
+      min--;
+      scs= 59;
+      if( min == 0){
+        min= 0;
+      }
+      if (min < 0){
+        clearInterval(time);
+      }
     }
-    if (min < 0){
-      clearInterval(time);
-    }
-  }
-
-}, 1000);
-
+  }, 1000);
 }
 
 function lBreak(){
   min = 15;
   scs = 59;
-
- tab =[];
-time  =setInterval(function (){
-
- timer = new Date();
-
-
-document.getElementById('timer').innerHTML = min +":"+scs;
-
-  scs--;
-  if( scs < 0){
-    min--;
-    scs= 59;
-    if( min == 0){
-      min= 0;
-    }
-    if( min < 0){
-      min = 0;
-      clearInterval(time);
-
-      document.getElementById('terminer').innerText="test";
-    }
+  tab =[];
+  time  =setInterval(function (){
+    timer = new Date();
+    document.getElementById('timer').innerHTML = min +":"+scs;
+    scs--;
+    if( scs < 0){
+      min--;
+      scs= 59;
+      if( min == 0){
+        min= 0;
       }
-    },1000)
-          }
+      if( min < 0){
+        min = 0;
+        clearInterval(time);
+        document.getElementById('terminer').innerText="test";
+      }
+    }
+  },1000)
+  console.log("longB : " + min + scs)
+}
 
 
-          function sBreak(){
-            min = 5;
-            scs = 59;
+function sBreak(){
+  min = 5;
+  scs = 59;
+  tab =[];
 
-           tab =[];
-          time  =setInterval(function (){
-
-           timer = new Date();
-
-
-          document.getElementById('timer').innerHTML = min +":"+scs;
-
-            scs--;
-            if( scs < 0){
-              min--;
-              scs= 59;
-              if( min == 0){
-                min= 0;
-              }
-              if( min < 0){
-                min = 0;
-                clearInterval(time);
-
-                document.getElementById('terminer').innerText="test";
-              }
-                }
-              },1000)
-                    }
+  time  =setInterval(function (){
+    timer = new Date();
+    document.getElementById('timer').innerHTML = min +":"+scs;
+    scs--;
+    if( scs < 0){
+      min--;
+      scs= 59;
+      if( min == 0){
+        min= 0;
+      }
+      if( min < 0){
+        min = 0;
+        clearInterval(time);
+        document.getElementById('terminer').innerText="test";
+      }
+    }
+  },1000)
+}
+console.log(min ,scs)
